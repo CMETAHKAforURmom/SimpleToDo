e3c8efae35c89589536b26889187277650eb22ed
CMETAHKAforURmom <isyperandrei@gmail.com>
2023-06-16 22:06:26 +0700

    Normally sign in and login method

--- app/src/main/java/com/andernam/simpletodo/ui/ToDoListUser.kt
+++
@@ -1,9 +0,0 @@
-package com.andernam.simpletodo.ui
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun ToDoListUser(navController: NavController) {
-
-}
